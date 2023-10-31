191926032524551697778ed96019e9a363fd8faf
victor braz <victor.90braz@gmail.com>
2023-10-31 17:10:02 +0100

    admin/posts/create

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -24,6 +24,10 @@ class PostController extends Controller
 
     public function create()
     {
+        if (auth()->guest()) {
+            abort(403);
+        }
+
         return view('posts.create');
     }
 }
