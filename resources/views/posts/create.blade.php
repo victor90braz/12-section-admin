b657a1c40179a65240a31d8857d5f478eaa8e1c7
victor braz <victor.90braz@gmail.com>
2023-11-01 10:49:23 +0100

    'slug' => ['required', Rule::unique('posts', 'slug')],

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -22,8 +22,8 @@
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
 
-                <label  class="block mb-2 uppercase font-bold text-xs text-gray-700" for="title">
-                    title
+                <label  class="block mb-2 uppercase font-bold text-xs text-gray-700 mt-3" for="title">
+                    slug
                 </label>
 
                 <input  class="border border-gray-400 p-2 w-full"
