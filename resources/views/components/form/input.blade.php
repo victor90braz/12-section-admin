d6325402d2dff8dd903a00ba918902340fb12fce
victor braz <victor.90braz@gmail.com>
2023-11-01 18:48:06 +0100

    refactor form

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,10 +1,7 @@
 @props(['name', 'type' => 'text'])
 
 <div class="mb-6">
-    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="{{ $name }}">
-        {{ ucwords($name) }}
-    </label>
-
+    <x-form.label name="{{ $name}}"/>
     <input class="border border-gray-400 p-2 w-full"
            type="{{ $type }}"
            name="{{ $name }}"
@@ -12,7 +9,6 @@
            required
            value="{{ old($name) }}"
     />
-
     <x-form.error name="{{ $name }}" />
 </div>
 
--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,9 +1,8 @@
 @props(['name'])
 
 <div class="mb-6">
-    <label  class="block mb-2 uppercase font-bold text-xs text-gray-700 mt-3" for="name">
-        {{ ucwords($name) }}
-    </label>
+
+    <x-form.label name="{{ $name}}"/>
 
     <textarea class="border border-gray-400 p-2 w-full" name="{{ $name }}" id="{{ $name }}" required>
         {{ old($name) }}
