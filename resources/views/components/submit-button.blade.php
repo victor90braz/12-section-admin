158fe57d87ed9e347291f7ffcf49ee087f8553e8
victor braz <victor.90braz@gmail.com>
2023-11-01 10:04:43 +0100

    create form

--- resources/views/components/submit-button.blade.php
+++ resources/views/components/submit-button.blade.php
@@ -1,5 +1,3 @@
-<button type="submit"
-        class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600"
->
+<button type="submit" {{ $attributes->merge(['class' => 'bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600']) }}>
     {{ $slot }}
 </button>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,5 +1,63 @@
 <x-layout>
     <section class="px-6 py-8">
-        <h1>Create Post</h1>
+        <x-panel class="max-w-sm mx-auto bg-gray-300">
+            <h1 class="text-center font-bold text-xl">Create</h1>
+
+            <form action="/admin/posts" method="POST">
+                @csrf
+
+                <label  class="block mb-2 uppercase font-bold text-xs text-gray-700" for="name">
+                    name
+                </label>
+
+                <input  class="border border-gray-400 p-2 w-full"
+                        type="text"
+                        name="name"
+                        id="name"
+                        required
+                        value="{{ old('name') }}"
+                />
+
+                @error('name')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+
+
+                <label  class="block mb-2 uppercase font-bold text-xs text-gray-700 mt-3" for="excerpt">
+                    excerpt
+                </label>
+
+                <input  class="border border-gray-400 p-2 w-full"
+                        type="text"
+                        name="excerpt"
+                        id="excerpt"
+                        required
+                        value="{{ old('excerpt') }}"
+                />
+
+                @error('excerpt')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+
+
+                <label  class="block mb-2 uppercase font-bold text-xs text-gray-700 mt-3" for="name">
+                    body
+                </label>
+
+                <input  class="border border-gray-400 p-2 w-full"
+                        type="text"
+                        name="body"
+                        id="body"
+                        required
+                        value="{{ old('body') }}"
+                />
+
+                @error('body')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+
+                <x-submit-button class="mt-3"> publish </x-submit-button>
+            </form>
+        </x-panel>
     </section>
 </x-layout>
