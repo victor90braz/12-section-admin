c3c960d3ab33b4dba0945bf58982ae8ffae2ecae
victor braz <victor.90braz@gmail.com>
2023-11-01 18:55:34 +0100

    refactor form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -14,23 +14,19 @@
                 <x-form.textarea name="excerpt"/>
                 <x-form.textarea name="body"/>
 
-                <section class="px-6 py-8">
-                    <label for="category_id" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        category
-                    </label>
+                <x-form.field>
+                    <x-form.label name="category"/>
 
-                    <select name="category_id" id="category_id">
-                        @foreach (App\Models\Category::all() as $category)
-                            <option value="{{ $category->id }}" {{ strval(old('category_id')) === strval($category->id) ? 'selected' : '' }}>
-                                {{ ucwords($category->name) }}
-                            </option>
-                        @endforeach
-                    </select>
+                        <select name="category_id" id="category_id">
+                            @foreach (App\Models\Category::all() as $category)
+                                <option value="{{ $category->id }}" {{ strval(old('category_id')) === strval($category->id) ? 'selected' : '' }}>
+                                    {{ ucwords($category->name) }}
+                                </option>
+                            @endforeach
+                        </select>
 
-                    @error('category_id')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </section>
+                        <x-form.error name='category'/>
+                </x-form.field>
 
                 <x-submit-button class="mt-3"> publish </x-submit-button>
             </form>
