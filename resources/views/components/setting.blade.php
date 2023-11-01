c29938eb1d54cef6f69aeee2717ca02ed3978673
victor braz <victor.90braz@gmail.com>
2023-11-01 20:08:58 +0100

    setting create form

--- resources/views/components/setting.blade.php
+++ resources/views/components/setting.blade.php
@@ -1,11 +1,28 @@
 @props(['heading'])
 
-<section class="mt-8 max-w-md mx-auto">
-    <h1 class="text-left p-2 font-bold text-xl">
+<section class="mt-8 max-w-4xl mx-auto">
+    <h1 class="text-left p-2 font-bold text-lg mb-8 pb-2 border-b">
        {{ $heading }}
     </h1>
 
-    <x-panel class="bg-gray-200">
-        {{ $slot }}
-    </x-panel>
+    <div class="flex">
+        <aside class="w-48">
+            <h4 class="font-semibold mb-4">Links</h4>
+            <ul>
+                <li>
+                    <a  href="/admin/posts/create"
+                        class="{{ Str::contains(request()->url(), '/admin/posts/create') ? 'text-blue-500' : '' }}">
+
+                        New Post
+                    </a>
+                </li>
+            </ul>
+        </aside>
+
+        <main class="flex-1">
+            <x-panel class="bg-gray-200">
+                {{ $slot }}
+            </x-panel>
+        </main>
+    </div>
 </section>
