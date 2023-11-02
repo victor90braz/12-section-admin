6fc423741771815ac5f3c9e0903403670f586a70
victor braz <victor.90braz@gmail.com>
2023-11-02 11:10:36 +0100

    <img src="{{asset('storage/' . $post->thumbnail)}}" alt="post created" class="rounded-xl">

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -6,6 +6,7 @@
             <x-form.input name="title" :value="old('title', $post->title)"/>
             <x-form.input name="slug" :value="old('slug', $post->slug)"/>
             <x-form.input name="thumbnail" type="file" :value="old('thumbnail', $post->thumbnail)"/>
+            <img src="{{asset('storage/' . $post->thumbnail)}}" alt="post created" class="rounded-xl">
             <x-form.textarea name="excerpt">{{ old('excerpt', $post->excerpt) }}</x-form.textarea>
             <x-form.textarea name="body">{{ old('body', $post->body) }}</x-form.textarea>
 
