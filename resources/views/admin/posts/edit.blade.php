1afb93dd40b3e7c90ff6d41b6ec346743edc9ace
victor braz <victor.90braz@gmail.com>
2023-11-02 10:47:31 +0100

    edit post

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,20 +1,23 @@
 <x-layout>
-    <x-setting :heading=" 'Edit Post' . $post->title ">
+    <x-setting :heading=" 'Edit Post: ' . $post->title ">
         <form action="/admin/posts" method="POST" enctype="multipart/form-data">
             @csrf
 
-            <x-form.input name="title"/>
-            <x-form.input name="slug"/>
-            <x-form.input name="thumbnail" type="file"/>
-            <x-form.textarea name="excerpt"/>
-            <x-form.textarea name="body"/>
+            <x-form.input name="title" :value="old('title', $post->title)"/>
+            <x-form.input name="slug" :value="old('slug', $post->slug)"/>
+            <x-form.input name="thumbnail" type="file" :value="old('thumbnail', $post->thumbnail)"/>
+            <x-form.textarea name="excerpt">{{ old('excerpt', $post->excerpt) }}</x-form.textarea>
+            <x-form.textarea name="body">{{ old('body', $post->body) }}</x-form.textarea>
 
             <x-form.field>
                 <x-form.label name="category"/>
 
                     <select name="category_id" id="category_id">
                         @foreach (App\Models\Category::all() as $category)
-                            <option value="{{ $category->id }}" {{ strval(old('category_id')) === strval($category->id) ? 'selected' : '' }}>
+                            <option
+                                    value="{{ $category->id }}"
+                                    {{ strval(old('category_id', $post->category_id)) === strval($category->id) ? 'selected' : '' }}
+                            >
                                 {{ ucwords($category->name) }}
                             </option>
                         @endforeach
--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -3,8 +3,14 @@
 <x-form.field>
     <x-form.label name="{{ $name }}"/>
 
-    <textarea class="border border-gray-400 p-2 w-full" name="{{ $name }}" id="{{ $name }}" required>
-        {{ old($name) }}
+    <textarea
+                class="border border-gray-400 p-2 w-full"
+                name="{{ $name }}" id="{{ $name }}"
+                required
+                {{ $attributes }}
+    >
+
+        {{ $slot ?? old($name) }}
     </textarea>
 
     <x-form.error name="{{ $name }}" />
