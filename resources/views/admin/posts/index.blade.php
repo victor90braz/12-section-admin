d8ff0a3ab54a3821bf85880db9cf94ef374b84d7
victor braz <victor.90braz@gmail.com>
2023-11-02 09:22:45 +0100

    delete session

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,5 +1,31 @@
 <x-layout>
     <x-setting heading="Delete Post" class="px-6 py-8">
-        delete session
+        <table class="min-w-full divide-y divide-gray-200">
+            <tbody class="bg-white divide-y divide-gray-200">
+                @foreach ($posts as $post)
+                <tr>
+                    <td class="px-6 py-4 white-space-nowrap">
+                        <div class="flex items-center">
+                            <div class="text-sm font-medium text-gray-900">
+                                {{ $post->title }}
+                            </div>
+                        </div>
+                    </td>
+
+                    <td class="px-6 py-4 white-space-nowrap">
+                            <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                Published
+                            </span>
+                    </td>
+
+                    <td class="px-6 py-4 white-space-nowrap text-right text-sm font-medium">
+                        <a href="/admin/posts/{{ $post->id }}/edit" class="text-blue-500 hover:text-blue-600">
+                            Edit
+                        </a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </x-setting>
 </x-layout>
