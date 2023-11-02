0dc193c0355c0cd5803602c008ee283f290a291f
victor braz <victor.90braz@gmail.com>
2023-11-02 08:58:43 +0100

    delete session

--- resources/views/components/setting.blade.php
+++ resources/views/components/setting.blade.php
@@ -2,7 +2,7 @@
 
 <section class="mt-8 max-w-4xl mx-auto">
     <h1 class="text-left p-2 font-bold text-lg mb-8 pb-2 border-b">
-       {{ $heading }}
+        {{ $heading }}
     </h1>
 
     <div class="flex">
@@ -10,17 +10,15 @@
             <h4 class="font-semibold mb-4">Links</h4>
             <ul>
                 <li>
-                    <a  href="/admin/posts/create"
-                        class="{{ Str::contains(request()->url(), '/admin/posts/create') ? 'text-blue-500' : '' }}">
-
+                    <a href="/admin/posts/create"
+                       class="{{ Str::contains(request()->url(), '/admin/posts/create') ? 'text-blue-500' : '' }}">
                         New Post
                     </a>
                 </li>
 
                 <li>
-                    <a  href="#"
-                        class="{{ Str::contains(request()->url(), '/admin/posts') ? 'text-blue-500' : '' }}">
-
+                    <a href="/admin/posts"
+                       class="{{ Str::contains(request()->url(), '/admin/posts') && !Str::contains(request()->url(), '/admin/posts/create') ? 'text-blue-500' : '' }}">
                         All Posts
                     </a>
                 </li>
