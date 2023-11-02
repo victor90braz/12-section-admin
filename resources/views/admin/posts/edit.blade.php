16f35c8087864bcced4d40274a678cb4fcc29cdb
victor braz <victor.90braz@gmail.com>
2023-11-02 11:27:47 +0100

    <img src="{{asset('storage/' . $post->thumbnail)}}" alt="post created" class="rounded-xl">

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,18 +1,29 @@
+To set a proper width for the image within your form, you can use Bootstrap classes to control the layout. Here's an updated version of your form that includes a proper width for the image:
+
+```html
 <x-layout>
-    <x-setting :heading=" 'Edit Post: ' . $post->title ">
+    <x-setting :heading="'Edit Post: ' . $post->title">
         <form action="/admin/posts" method="POST" enctype="multipart/form-data">
             @csrf
 
             <x-form.input name="title" :value="old('title', $post->title)"/>
             <x-form.input name="slug" :value="old('slug', $post->slug)"/>
-            <x-form.input name="thumbnail" type="file" :value="old('thumbnail', $post->thumbnail)"/>
-            <img src="{{asset('storage/' . $post->thumbnail)}}" alt="post created" class="rounded-xl">
+
+            <div class="flex mt-6 gap-2 flex-col space-y-2">
+                <div class="flex-1">
+                    <x-form.input name="thumbnail" type="file" :value="old('thumbnail', $post->thumbnail)" class="block w-full px-4 py-2 leading-5 text-gray-700 placeholder-gray-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50 border border-gray-300 rounded-md transition duration-150 ease-in-out sm:text-sm sm:leading-5"/>
+                </div>
+                <div class="w-1/4 flex-1">
+                    <img src="{{ asset('storage/' . $post->thumbnail) }}" alt="post created" class="rounded-xl border border-gray-300 w-full max-h-40">
+                </div>
+            </div>
+
             <x-form.textarea name="excerpt">{{ old('excerpt', $post->excerpt) }}</x-form.textarea>
             <x-form.textarea name="body">{{ old('body', $post->body) }}</x-form.textarea>
 
             <x-form.field>
                 <x-form.label name="category"/>
-                <select name="category_id" id="category_id">
+                <select name="category_id" id="category_id" class="w-full px-4 py-2 text-gray-700 border border-gray-300 rounded-md">
                     @foreach (App\Models\Category::all() as $category)
                         <option
                                 value="{{ $category->id }}"
@@ -25,7 +36,10 @@
                 <x-form.error name='category'/>
             </x-form.field>
 
-            <x-submit-button class="mt-3"> publish </x-submit-button>
+            <x-submit-button class="mt-3">Publish</x-submit-button>
         </form>
     </x-setting>
 </x-layout>
+```
+
+In this code, I've set a `w-1/4` (25% width) class to the `div` containing the image, which limits the width of the image. I've also added `max-h-40` (maximum height) to control the image's height. You can adjust the values as needed to achieve the desired image size and layout.
